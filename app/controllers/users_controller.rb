4f3228c705643c741eb5d3c0b3fd01f734feb799
磯部浩樹 <isobehiroki@MacBook-Pro.local>
2020-08-10 19:13:30 +0900

    [Add]プロフィール編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @tweets = @user.tweets
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:profile_image, :name)
+  end
 end
